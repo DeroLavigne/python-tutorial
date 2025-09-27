8a259b7a9b04432ec3e64addc4eed371d7c5ec56
DeroLavigne <David.Martinez.Saggiorato@gmail.com>
2025-09-27 00:44:14 -0500

    Primer commit: proyecto base

---
+++ main.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+favorite_color = input("What is your favorite color? ")
+age = input("What is your age? ")
+print("Hello,", name + "!", "Your favorite color is", favorite_color + ".", "Your age is", age)
